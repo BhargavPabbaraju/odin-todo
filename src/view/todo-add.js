d92ba1baf5fb720b8c3b6c6fd549175ba37cb5e2
BhargavPabbaraju <bhargav.n.shyam@gmail.com>
2025-01-20 15:42:39 -0500

    Style add todo form

--- src/view/todo-add.js
+++ src/view/todo-add.js
@@ -1,10 +1,12 @@
 import { Priority } from '../todos/common';
 import * as icons from './icons';
+import './todo-add.css';
 
 function renderCloseButton(onClose){
     const div = document.createElement("div");
+    div.classList.add("row");
+    div.classList.add("end");
     const button = icons.getCross();
-    button.classList.add("close-button");
     button.addEventListener("click",onClose);
     div.appendChild(button);
     return div;
@@ -13,7 +15,7 @@ function renderCloseButton(onClose){
 
 function renderTodoTitle(){
     const div = document.createElement("div");
-    div.classList.add("title");
+    div.classList.add("row");
     div.classList.add("required");
 
     const label = document.createElement("label");
@@ -35,6 +37,7 @@ function renderTodoTitle(){
 function renderDescription(){
     const div = document.createElement("div");
     div.classList.add("description");
+    div.classList.add("column");
 
     const label = document.createElement("label");
     label.htmlFor = "description";
@@ -42,7 +45,7 @@ function renderDescription(){
 
     const input = document.createElement("textarea");
     input.name = "description";
-    input.innerText = "Sample description";
+    input.placeholder = "Sample description";
 
     div.appendChild(label);
     div.appendChild(input);
@@ -52,8 +55,7 @@ function renderDescription(){
 
 
 function renderPriority(){
-    const div = document.createElement("div");
-    div.classList.add("priority");
+    const span = document.createElement("span");
 
     const label = document.createElement("label");
     label.htmlFor = "priority";
@@ -71,17 +73,18 @@ function renderPriority(){
         option.value = priority;
         option.innerText = priorityLabel;
         input.appendChild(option);
-    })
+    });
+    input.value = Priority.UNASSIGNED;
 
-    div.appendChild(label);
-    div.appendChild(input);
+    span.appendChild(label);
+    span.appendChild(input);
 
-    return div;
+    return span;
 }
 
 function renderDueDate(){
-    const div = document.createElement("div");
-    // div.classList.add("due-date");
+    const span = document.createElement("span");
+    span.classList.add("row");
 
     const label = document.createElement("label");
     label.htmlFor = "due-date";
@@ -91,22 +94,22 @@ function renderDueDate(){
     input.name = "due-date";
     input.type = "date";
 
-    div.appendChild(label);
-    div.appendChild(input);
+    span.appendChild(label);
+    span.appendChild(input);
 
-    return div;
+    return span;
 }
 
 function renderNotes(){
     const div = document.createElement("div");
-
+    div.classList.add("column");
     const label = document.createElement("label");
     label.htmlFor = "notes";
     label.innerText = "Notes";
 
     const input = document.createElement("textarea");
     input.name = "notes";
-    input.innerText = "Notes";
+    input.placeholder = "Sample Notes";
 
     div.appendChild(label);
     div.appendChild(input);
@@ -114,8 +117,14 @@ function renderNotes(){
     return div;
 }
 
-function renderChecklist(){
+function renderAddButton(){
+    const input = document.createElement("input");
+    input.classList.add("add-button");
+    input.type = "submit";
+    input.value = "Add";
+    return input;
 }
+
 
 
 export function renderAddTodoForm(onCloseAddTodo){
@@ -129,9 +138,13 @@ export function renderAddTodoForm(onCloseAddTodo){
     form.appendChild(renderCloseButton(onCloseAddTodo));
     form.appendChild(renderTodoTitle());
     form.appendChild(renderDescription());
-    form.appendChild(renderPriority());
-    form.appendChild(renderDueDate());
+    const div = document.createElement("div");
+    div.classList.add("row");
+    div.appendChild(renderPriority());
+    div.appendChild(renderDueDate());
+    form.appendChild(div);
     form.appendChild(renderNotes());
+    form.appendChild(renderAddButton());
     
     content.appendChild(form);
 
