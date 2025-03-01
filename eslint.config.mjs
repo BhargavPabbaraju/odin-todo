c204ad19fe3983e645b883fee8460c6bc756f3d1
BhargavPabbaraju <bhargav.n.shyam@gmail.com>
2025-02-28 21:46:21 -0500

    Add a script to test and add jest to eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,17 @@ import eslintConfigPrettier from "eslint-config-prettier";
 export default [
   { files: ["**/*.{js,mjs,cjs,jsx}"] },
   { languageOptions: { globals: globals.browser } },
+  {
+    overrides: [
+      {
+        files: ["tests/**/*"],
+        plugins: ["jest"],
+        env: {
+          "jest/globals": true,
+        },
+      },
+    ],
+  },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
   eslintConfigPrettier,
